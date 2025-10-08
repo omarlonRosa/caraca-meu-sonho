0e462bebc4913deeb7a5741763dbca35f315af61
Marlon Rosa <rosa.marlon@icloud.com>
2025-10-08 19:12:13 -0300

    fix: corrigir erros de TypeScript

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,26 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom' // 1. Importe o BrowserRouter
+import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
-
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-
-if (!GOOGLE_CLIENT_ID) {
-  throw new Error("Client ID do Google não encontrado. Verifique o arquivo .env.local");
-}
-
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || '';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-    <BrowserRouter>
-       <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-    </GoogleOAuthProvider>
+    {GOOGLE_CLIENT_ID ? (
+      <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+        <BrowserRouter>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </BrowserRouter>
+      </GoogleOAuthProvider>
+    ) : (
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    )}
   </React.StrictMode>,
 )
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
-    host: true, // Permite que o servidor seja acessado pela rede
+    host: true,
     hmr: {
       host: 'localhost',
     },
